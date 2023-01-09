875d40f03bc1ab93fcdc4cd3279160000558f718
ajarwee <ajarwee@byui.edu>
2023-01-09 13:05:39 -0700

    uploadwebpage

---
+++ main.js
@@ -0,0 +1,8 @@
+let date = new Date();
+let year = date.getFull year();
+
+document.querySelector('h1').innerHTML='&copy;' + year;
+
+let currendate = docuemtn.lastModified;
+
+document.querySelector('dive').textContent = `lastupdate: + ${currendate}`;
