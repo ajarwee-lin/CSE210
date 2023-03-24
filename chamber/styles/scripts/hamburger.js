01a8cbcb5c52c75d50c3023f4e96b0968b7530e9
ajarwee <ajarwee@byui.edu>
2023-03-24 12:26:03 -0600

    changes

--- chamber/styles/scripts/hamburger.js
+++ chamber/styles/scripts/hamburger.js
@@ -1,5 +1,5 @@
 // Hamburger Menu
-
+// let allows us to declare a variable
 let hamburger = document.querySelector('#hamburger');
 let nav = document.querySelector('header');
 
@@ -7,3 +7,14 @@ hamburger.addEventListener('click', () => {
     nav.classList.toggle('responsive');
 
 });
+
+// Practice
+let myInt = 1; // int
+let myString = "Lincoln"; // string
+let myBool = false; //  boolean
+
+if (myBool) {
+    let myInt2 = 2;   
+}
+
+let myBanner = document.querySelector('.banner');
