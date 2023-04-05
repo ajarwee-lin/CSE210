b1376dbddadc6d90b4d2cad9f20b78f6ce881ab1
ajarwee <ajarwee@byui.edu>
2023-04-05 16:40:40 -0600

    changes

--- Bountiful-foods/scripts/drink.js
+++
@@ -1,6 +0,0 @@
-if (!localStorage.getItem('numDrinks')){
-    localStorage.setItem('numDrinks', 0);
-  }
-  
-  
-  document.querySelector("#numDrinks").innerHTML = localStorage.getItem('numDrinks');
